8dfe5351cd05b1c2d67b5049dc3b9e903fe4f358
Zahid Zeeshan <zzeeshan0503@gmail.com>
2025-03-09 03:42:58 +0500

    error fixing for live production env

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ pip install --upgrade pip
 pip install gunicorn
 pip install django
 pip install whitenoise
+pip install django-cors-headers
 python manage.py collectstatic --noinput
 pip install Pillow
