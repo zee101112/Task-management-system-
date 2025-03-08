8e2b11406885f1333e22afaa4662793335172757
Zahid Zeeshan <zzeeshan0503@gmail.com>
2025-03-09 01:16:22 +0500

    change in helpers.py file for email recovery updates urls

--- TMapp/helpers.py
+++ TMapp/helpers.py
@@ -1,34 +1,39 @@
 from django.core.mail import send_mail
-
 from django.conf import settings
 
-
-def send_forget_password_email(email,token):
-    subject = 'Your Forget Password Link'
-    message = f'Hi, Click on the following link to rest your password http://127.0.0.1:8000/change-password/{token}'
-    mailfrom = settings.EMAIL_HOST_USER
-    receipt_mail = [email]
-    send_mail(subject,message,mailfrom,receipt_mail)
-    return True
-
-
-def send_Register_email(email, user_name,token):
+def send_forget_password_email(request, email, token):
+    subject = 'Your Password Reset Link'
+    reset_url = request.build_absolute_uri(f'/change-password/{token}')
+    message = f'Hi, Click on the following link to reset your password: {reset_url}'
     
-    subject = 'Thanks For Registering With Us Verify your Account'
-    message = f'Hi, {user_name} We are Very happy To see you here at our Task Managemant System. Click the link to verify your account: http://127.0.0.1:8000/verify/{token}'
-    mailfrom = settings.EMAIL_HOST_USER
-    receipt_mail = [email]
-    send_mail(subject, message, mailfrom, receipt_mail)
+    mail_from = settings.EMAIL_HOST_USER
+    recipient_mail = [email]
+    
+    send_mail(subject, message, mail_from, recipient_mail)
     return True
 
-def send_Account_Verification_email(email, u_name, f_name):
-    subject = 'Verification of your Account'
-    message = f'Hi, {f_name} your account was Verified By the admin you can now use your account. For more info, Contact us.'
-    mailfrom = settings.EMAIL_HOST_USER
-    receipt_mail = email
+
+def send_register_email(request, email, user_name, token):
+    subject = 'Thanks for Registering - Verify Your Account'
+    verify_url = request.build_absolute_uri(f'/verify/{token}')
+    message = f'Hi {user_name},\n\nWe are very happy to have you on our Task Management System.\nClick the link to verify your account: {verify_url}'
+    
+    mail_from = settings.EMAIL_HOST_USER
+    recipient_mail = [email]
+    
+    send_mail(subject, message, mail_from, recipient_mail)
+    return True
+
+
+def send_account_verification_email(email, u_name, f_name):
+    subject = 'Your Account Has Been Verified'
+    message = f'Hi {f_name},\n\nYour account has been verified by the admin. You can now log in and use your account. For more info, feel free to contact us.'
+    
+    mail_from = settings.EMAIL_HOST_USER
+    recipient_mail = [email]
 
     try:
-        send_mail(subject, message, mailfrom, [receipt_mail])
+        send_mail(subject, message, mail_from, [recipient_mail])
         return True
     except Exception as e:
         print(f"An error occurred while sending the email: {str(e)}")
